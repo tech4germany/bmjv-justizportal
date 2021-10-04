da8db07336cb5bc253be3b3f6f33f6c7c93cd64f
Michael Laßmann <michael.lassmann@tech4germany.org>
2021-10-04 17:45:58 +0200

    Markdown support for SolutionExplorer

--- src/pages/SolutionExplorer.tsx
+++ src/pages/SolutionExplorer.tsx
@@ -20,13 +20,13 @@ import {
   useDisclosure,
   VStack,
 } from '@chakra-ui/react';
+import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
 import * as React from 'react';
 import { ReactNode } from 'react';
 import { FaInfoCircle } from 'react-icons/fa';
+import ReactMarkdown from 'react-markdown';
 import { Link as ReactLink } from 'react-router-dom';
-
 import parseData from '../logic/KMParser';
-
 
 interface StatsCardProps {
   title: string;
@@ -36,69 +36,75 @@ interface StatsCardProps {
 }
 function StatsCard(props: StatsCardProps) {
   const { title, info, icon, link } = props;
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const green = useColorModeValue('green.500', 'green.300');
-
-
 
   return (
     <>
-
       <Stat
         shadow={'0px 0px 0.5em gray'}
         border={'1px solid'}
         borderColor={useColorModeValue('gray.200', 'whiteAlpha.400')}
         bg={useColorModeValue('gray.100', 'whiteAlpha.200')}
         rounded={'lg'}
-        _hover={
-          {
-            transitionDuration: '0.2s',
-            bg: useColorModeValue('gray.200', 'whiteAlpha.400')
-          }
-        }
-
+        _hover={{
+          transitionDuration: '0.2s',
+          bg: useColorModeValue('gray.200', 'whiteAlpha.400'),
+        }}
       >
         <>
           <Box
             display={info == undefined ? 'none' : ''}
-            as='a'
-            href='#'
-            pos='absolute'
-            width='auto'
+            as="a"
+            href="#"
+            pos="absolute"
+            width="auto"
             top={1.5}
             right={1.5}
             color={useColorModeValue('gray.700', 'whiteAlpha.400')}
             _hover={{
               transitionDuration: '0.2s',
-              color: green
+              color: green,
             }}
           >
-            <FaInfoCircle size='1.4em' onClick={(e) => { e.stopPropagation(); onOpen(); }} />
+            <FaInfoCircle
+              size="1.4em"
+              onClick={(e) => {
+                e.stopPropagation();
+                onOpen();
+              }}
+            />
           </Box>
 
-          <Flex justifyContent={'space-between'} flexDir='column' alignItems='center' as={ReactLink} to={link} p={3}>
-            <Box my={'auto'} color={green} padding='0.8em'>
+          <Flex
+            justifyContent={'space-between'}
+            flexDir="column"
+            alignItems="center"
+            as={ReactLink}
+            to={link}
+            p={3}
+          >
+            <Box my={'auto'} color={green} padding="0.8em">
               {icon}
             </Box>
-            <StatLabel fontSize={'lg'} >
-              {title}
-            </StatLabel>
+            <StatLabel fontSize={'lg'}>{title}</StatLabel>
           </Flex>
         </>
       </Stat>
-
       <Modal closeOnOverlayClick={true} isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>
             <HStack>
-              <FaInfoCircle size='1.4em' />
+              <FaInfoCircle size="1.4em" />
               <Text>Info</Text>
             </HStack>
           </ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
-            <Text>{info}</Text>
+            <ReactMarkdown components={ChakraUIRenderer()}>
+              {info != undefined ? info : ''}
+            </ReactMarkdown>
           </ModalBody>
 
           <ModalFooter>
@@ -106,43 +112,55 @@ function StatsCard(props: StatsCardProps) {
           </ModalFooter>
         </ModalContent>
       </Modal>
-    </ >
+    </>
   );
 }
 
-
-
 interface FeatureProps {
-  id: string | null
+  id: string | null;
 }
 
 export const SolutionExplorer = (props: FeatureProps) => {
-  const { id } = props
+  const { id } = props;
   const data = parseData(id);
 
   return (
     <Box textAlign="left" fontSize="xl">
-
       <Spacer height={{ base: '1em', md: '1em' }} />
 
       <VStack padding={'1em'} spacing={'2em'}>
-
-        <Heading> In welchem Bereich Ihres Lebens haben Sie ein Problem?</Heading>
+        <Heading>
+          {' '}
+          In welchem Bereich Ihres Lebens haben Sie ein Problem?
+        </Heading>
 
         <SimpleGrid columns={{ base: 2, md: 3 }} spacing={{ base: 5, lg: 8 }}>
           {data!.children!.map((child) => (
-            <StatsCard link={`${child.children == undefined ? '/entitlement' : ''}?id=${child.id}`} {...child} />
+            <StatsCard
+              link={`${child.children == undefined ? '/entitlement' : ''}?id=${
+                child.id
+              }`}
+              {...child}
+            />
           ))}
         </SimpleGrid>
 
         <HStack>
-          <Button rounded={'full'} colorScheme='gray' as={ReactLink} to='/'>Zurück</Button>
-          <Button rounded={'full'} display='none' colorScheme='green' bg={useColorModeValue('green.500', 'green.300')} as={ReactLink} to='/entitlement'>Weiter</Button>
+          <Button rounded={'full'} colorScheme="gray" as={ReactLink} to="/">
+            Zurück
+          </Button>
+          <Button
+            rounded={'full'}
+            display="none"
+            colorScheme="green"
+            bg={useColorModeValue('green.500', 'green.300')}
+            as={ReactLink}
+            to="/entitlement"
+          >
+            Weiter
+          </Button>
         </HStack>
       </VStack>
     </Box>
-  )
-}
-
-
-
+  );
+};
