53a65b40943c566e11c710c73a7a495312ec6f69
Michael Laßmann <michael.lassmann@tech4germany.org>
2021-10-06 10:10:57 +0200

    Prettier formating

--- src/logic/KMParser.tsx
+++ src/logic/KMParser.tsx
@@ -4,49 +4,48 @@ import * as Icons from 'react-icons/fa';
 
 import Data from '../data_parser/data.json';
 
-
 interface DynamicFaIconProps {
-  name: string
+  name: string;
 }
 
 const DynamicFaIcon = (props: DynamicFaIconProps) => {
   const IconComponent: any = Icons[props.name];
 
-  if (!IconComponent) { // Return a default one
-    return <Icons.FaBeer size='3.5em' />;
+  if (!IconComponent) {
+    // Return a default one
+    console.log(`[Warrning] Cloud not find icon '${props.name}'`);
+    return <Icons.FaBeer size="3.5rem" />;
   }
 
-  return <IconComponent size='3.5em' />;
+  return <IconComponent size="3.5rem" />;
 };
 
 export interface MMNode {
-  id: string,
-  title: string,
-  icon?: ReactNode,
-  info?: string,
-  children?: MMNode[]
+  id: string;
+  title: string;
+  icon?: ReactNode;
+  info?: string;
+  children?: MMNode[];
 }
 
 export class MMGraph {
-  
-  private parent: { [id: string] : MMNode } = {};
-  private nodes: { [id: string] : MMNode } = {};
+  private parent: { [id: string]: MMNode } = {};
+  private nodes: { [id: string]: MMNode } = {};
   private root: MMNode;
+  private numberOfNotes = 0;
 
   constructor() {
-    
-    this.root = { 'title': '', 'id': '' };
-    
+    this.root = { title: '', id: '' };
   }
 
   initialize() {
-    let currentInput : any = Data['root']
-    let currentNode: MMNode = { 'title': currentInput['data']['text'], 'id': currentInput['data']['id'] };
+    let currentInput: any = Data['root'];
+    let currentNode: MMNode = { title: currentInput['data']['text'], id: currentInput['data']['id'] };
     this.root = this.traverseMindMap(Data['root']);
   }
 
   getNode(id: string | null): MMNode {
-    if(id != null) {
+    if (id != null && id in this.nodes) {
       return this.nodes[id];
     } else {
       return this.root;
@@ -54,20 +53,21 @@ export class MMGraph {
   }
 
   getParent(id: string | null): MMNode | undefined {
-    if(id != null && this.parent[id] != null) {
+    if (id != null && id in this.parent) {
       return this.parent[id];
     } else {
       return undefined;
     }
   }
-  
-  private traverseMindMap(currentInput: any) : MMNode {
-    let currentNode: MMNode = { 'title': currentInput['data']['text'], 'id': currentInput['data']['id'] };
+
+  private traverseMindMap(currentInput: any): MMNode {
+    let currentNode: MMNode = { title: currentInput['data']['text'], id: currentInput['data']['id'] };
+
     if (currentInput.data.hasOwnProperty('note')) {
       let lines: string[] = currentInput['data']['note'].split('\n');
 
       if (lines[0].includes('{ICON:')) {
-        currentNode.icon = (<DynamicFaIcon name={lines[0].substring(6, lines[0].length - 1)} />);
+        currentNode.icon = <DynamicFaIcon name={lines[0].substring(6, lines[0].length - 1)} />;
         lines.splice(0, 1);
       }
 
@@ -77,7 +77,12 @@ export class MMGraph {
 
     if (Array.isArray(currentInput['children']) && currentInput['children'].length != 0) {
       currentNode.children = [];
-      for (const child of currentInput['children']) {
+      for (let child of currentInput['children']) {
+        if (child['data']['id'] in this.nodes) {
+          // FIx reappearing IDs
+          child['data']['id'] += 'X';
+        }
+
         this.parent[child['data']['id']] = currentNode;
         let childObject = this.traverseMindMap(child);
         currentNode.children.push(childObject);
