011011ddb45e4b7f99c2cdfbc79d099248d7db0d
Michael Laßmann <michael.lassmann@tech4germany.org>
2021-10-20 18:36:49 +0200

    Fix iframe height in BRYTER page

--- src/pages/BRYTER.tsx
+++ src/pages/BRYTER.tsx
@@ -1,11 +1,34 @@
 import { Box } from '@chakra-ui/react';
 import * as React from 'react';
+import { useEffect } from 'react';
 
-interface FeatureProps {}
+interface FeatureProps {
+  id: string;
+}
 
-export const Bryter = (props: FeatureProps) => {
+export const Bryter = ({ id, ...rest }: FeatureProps) => {
+  const type: string = '';
+
+  const bryterURLs = {
+    mangel: '',
+  };
+
+  useEffect(() => {
+    function handleResize() {
+      if (
+        document.getElementsByTagName('iframe')[0].offsetHeight != document.getElementById('iframeBox')!.offsetHeight
+      ) {
+        document.getElementsByTagName('iframe')[0].height = String(document.getElementById('iframeBox')!.offsetHeight);
+      }
+    }
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+  });
+
   return (
-    <Box flex="1">
+    <Box id="iframeBox" flex="1">
       <iframe
         title="Bryter"
         src="https://openprogram.bryter.io/s/KGITdkStRbemIKQWkrJH3A/klagegenerierung-mietmangel?antr[…]%C3%A4ngelbeseitigung,%20Mietminderung&mangelBehoben=false"
@@ -13,7 +36,6 @@ export const Bryter = (props: FeatureProps) => {
           //o.o.style.height = o.contentWindow.document.body.scrollHeight + 'px';
         }}
         style={{
-          height: '100%',
           minHeight: '40em',
           width: '100%',
           display: 'inline',
