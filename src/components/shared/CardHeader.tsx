89b84d2c7bec5a312c3c39d5ce308fa316b2a9b0
Michael Laßmann <michael.lassmann@tech4germany.org>
2021-10-20 11:43:51 +0200

    Switch Icon and Text in SolutionExplorer

--- src/components/shared/CardHeader.tsx
+++ src/components/shared/CardHeader.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading } from '@chakra-ui/react';
+import { Flex, Heading, Spacer } from '@chakra-ui/react';
 import * as React from 'react';
 
 interface Props {
@@ -8,11 +8,11 @@ interface Props {
 
 export const CardHeader = ({ title, action }: Props) => {
   return (
-    <Flex align="center" justify="space-between" px="6" py="4" borderBottomWidth="1px">
-      <Heading as="h2" fontSize="lg">
+    <Flex align="center" justify="start" alignItems="center" px="6" py="4" borderBottomWidth="1px">
+      {action}
+      <Heading as="h2" px={3} paddingBottom={1} fontSize="lg">
         {title}
       </Heading>
-      {action}
     </Flex>
   );
 };
