f367bac7a00dff4bcddb8040f63bd6615f4acecf
Nils Thamm <nils.thamm@student.hpi.de>
2021-10-21 12:08:10 +0200

    Added missing changes

---
+++ src/Routes.tsx
@@ -0,0 +1,10 @@
+export enum Routes {
+  Disclaimer = 'disclaimer',
+  SolutionExplorer = 'loesungs-finder',
+  Exit = 'exit',
+  ZPOInformation = 'zivil-prozess-info',
+  PossibleEntitlements = 'moegliche-ansprueche',
+  NextSteps = 'naechste-schritte',
+  Bryter = 'formular',
+  ConsultationOffers = 'beratungsangebote',
+}
