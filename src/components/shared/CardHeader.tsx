6bf50d042d87108ec79165fcddf00de05b4d4b8a
Michael Laßmann <michael.lassmann@tech4germany.org>
2021-10-22 19:06:26 +0200

    Improved Cards

--- src/components/shared/CardHeader.tsx
+++ src/components/shared/CardHeader.tsx
@@ -1,18 +1,17 @@
 import { Flex, Heading } from '@chakra-ui/react';
 import * as React from 'react';
+import { IconType } from 'react-icons';
 
 interface Props {
   title: string;
-  iconLeft?: React.ReactNode;
+  IconLeft?: IconType;
 }
 
-export const CardHeader = ({ title, iconLeft }: Props) => {
-  const IconComponent: any = iconLeft;
-
+export const CardHeader = ({ title, IconLeft }: Props) => {
   return (
-    <Flex align="center" justify="start" alignItems="center" px="6" py="4" borderBottomWidth="1px">
-      {IconComponent ?? <IconComponent size="1.2em" />}
-      <Heading as="h2" px={3} paddingBottom={1} fontSize="lg">
+    <Flex align="center" justify="start" alignItems="center" px="6" py="4" spacin borderBottomWidth="1px">
+      {IconLeft && <IconLeft size="1.2em" />}
+      <Heading as="h2" paddingBottom={1} ps={IconLeft && 3} fontSize="lg">
         {title}
       </Heading>
     </Flex>
