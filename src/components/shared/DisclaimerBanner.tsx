7b9901ba2cec1dce918d8ecbf44246af454f710e
Michael Laßmann <git@mlassmann.de>
2021-11-03 22:47:51 +0100

    Fix Banner

--- src/components/shared/DisclaimerBanner.tsx
+++ src/components/shared/DisclaimerBanner.tsx
@@ -7,36 +7,27 @@ export const DisclaimerBanner = ({ ...props }) => {
   const [showBanner, setShowBanner] = useState(true);
   return (
     <Box
-      top={0}
       width="100%"
       bg={'gray.200'}
-      py={{ base: 4 }}
-      px={{ base: 4 }}
+      p={4}
       borderBottom={1}
       borderStyle={'solid'}
       borderColor={'gray.200'}
       display={showBanner ? 'inherit' : 'none'}>
-      <Flex alignSelf="center" justify="center" align="start" width="100%" flex={1}>
-        <Flex align={'center'} justify={'center'} spacing={1} maxW={'60rem'} flex={1}>
-          {/* <IconButton variant="ghost" aria-label="open menu" icon={<AiFillCloseCircle />} /> */}
-          <Text fontWeight="bold" textAlign="center" fontSize={'small'} flex={1}>
-            Diese Seite ist ein Prototyp des{' '}
-            <Link href="https://tech.4germany.org/project/justizsportal/">Tech4Germany Fellowships 2021</Link> und
-            bietet keine verbindlichen Rechtsinformationen.
-          </Text>
-        </Flex>
-      </Flex>
-      <Box h="0" w="0">
-        <Box width="max-content">
-          <IconButton
-            aria-label=""
-            icon={<AiFillCloseCircle />}
-            variant="ghost"
-            onClick={() => setShowBanner(false)}
-            position="relative"
-            transform="translateX(-75%) translateY(-25%)"
-          />
-        </Box>
+      <Text fontWeight="bold" textAlign="center" fontSize={'small'} flex={1}>
+        Diese Seite ist ein Prototyp des{' '}
+        <Link href="https://tech.4germany.org/project/justizsportal/">Tech4Germany Fellowships 2021</Link> und bietet
+        keine verbindlichen Rechtsinformationen.
+      </Text>
+      <Box position="relative">
+        <IconButton
+          aria-label=""
+          icon={<AiFillCloseCircle />}
+          variant="ghost"
+          onClick={() => setShowBanner(false)}
+          position="absolute"
+          transform="translateX(-75%) translateY(-25%)"
+        />
       </Box>
     </Box>
   );
